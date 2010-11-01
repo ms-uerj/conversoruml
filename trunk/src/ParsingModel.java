5b819c1dcc021460c34138fb61e2e2756a860f2e
sergiohenrique1989 <sergiohenrique1989@0c1d6bcd-298f-3506-d018-7a1f83f52972>
2010-11-01 17:03:21 +0000

    -Adicionado suporte a namespaces.
    -Caso infantil xmiToUml resolvido.

--- trunk/src/ParsingModel.java
+++ trunk/src/ParsingModel.java
@@ -3,6 +3,9 @@ import java.util.*;
 import org.jdom.*;
 import org.jdom.output.XMLOutputter;
 
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.xpath.*;
+
 public class ParsingModel implements ParsingModelInterface {
 	ArrayList parsingObservers = new ArrayList();
 	
@@ -26,29 +29,31 @@ public class ParsingModel implements ParsingModelInterface {
 	}
 	
 	public void xmiToUml(File file){
-		Element umlModel = new Element("Model");
+		Element umlModel = new Element("Model","uml","http://www.eclipse.org/uml2/3.0.0/UML");
 		Element packagedElement = new Element("packagedElement");
 		Element packagedElement2 = new Element("packagedElement");
 		Element ownedAttribute = new Element("ownedAttribute");
 		Element lowerValue = new Element("lowerValue");
 		
-		umlModel.setAttribute("version","2.1");
-		umlModel.setAttribute("id","_zv37QN7mEd-oHKAY1dg54A");
+		Namespace namespace = Namespace.getNamespace("xmi", "http://schema.omg.org/spec/XMI/2.1");
 		
-		packagedElement.setAttribute("type","umlPrimitiveType");
-		packagedElement.setAttribute("id","_6uH_cd7pEd-9r-9AFaL-Dg");
+		umlModel.setAttribute("version","2.1", namespace);
+		umlModel.setAttribute("id","_zv37QN7mEd-oHKAY1dg54A", namespace);
+		
+		packagedElement.setAttribute("type","uml:PrimitiveType", namespace);
+		packagedElement.setAttribute("id","_6uH_cd7pEd-9r-9AFaL-Dg", namespace);
 		packagedElement.setAttribute("name","int");
 		
-		packagedElement2.setAttribute("type","uml:Class");
-		packagedElement2.setAttribute("id","_FpEKYN7nEd-oHKAY1dg54A");
+		packagedElement2.setAttribute("type","uml:Class", namespace);
+		packagedElement2.setAttribute("id","_FpEKYN7nEd-oHKAY1dg54A", namespace);
 		packagedElement2.setAttribute("name","Classe1");
 		
-		ownedAttribute.setAttribute("id","_jt4B0N7nEd-oHKAY1dg54A");
+		ownedAttribute.setAttribute("id","_jt4B0N7nEd-oHKAY1dg54A", namespace);
 		ownedAttribute.setAttribute("name","atrib1");
 		ownedAttribute.setAttribute("type","_6uH_cd7pEd-9r-9AFaL-Dg");
 		
-		lowerValue.setAttribute("type","uml:LiteralInteger");
-		lowerValue.setAttribute("id","_AXOV8N7qEd-9r-9AFaL-Dg");
+		lowerValue.setAttribute("type","uml:LiteralInteger",namespace);
+		lowerValue.setAttribute("id","_AXOV8N7qEd-9r-9AFaL-Dg", namespace);
 		
 		ownedAttribute.addContent(lowerValue);
 		packagedElement2.addContent(ownedAttribute);

75db26c154907479e6ff3fca4b44400697cb8b52
nhithael@gmail.com <nhithael@gmail.com@0c1d6bcd-298f-3506-d018-7a1f83f52972>
2010-11-04 02:23:48 +0000

    Realizado a cópia da versão do 2º seminário.

---
+++ tags/seminario_2/src/ParsingModelInterface.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+
+public interface ParsingModelInterface {
+	
+	/* Estes métodos permitem que a visualização e o controlador 
+	 * recebam informações sobre o estado e tornem-se observadores */
+	void initialize();
+	void registerObserver(ParsingObserver o);
+	void removeObserver(ParsingObserver o);
+	
+	/* Estes métodos são responsáveis pela conversão */
+	void umlToXmi(File file, File directory);
+	boolean xmiToUml(File file, File directory);
+}
